af8e7b971bb7f4ac1f68d40786bb529cf3d300e1
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-31 01:38:11 +0800

    Add files via upload

---
+++ Mutator/fuMutatorPreviewTargetBlock_20220830/toolbox.js
@@ -0,0 +1,8 @@
+var catTEST ='<xml>'+
+'  <category name="TEST" id="catTEST" colour="170">'+
+'	  <block type="test_statement">'+
+'	  </block>'+
+'	  <block type="test_preview">'+
+'	  </block>'+
+'  </category>'+
+'</xml>';
